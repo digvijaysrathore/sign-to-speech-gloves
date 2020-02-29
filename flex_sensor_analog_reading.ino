872a03156dc7dad5ba9966adc4285c67c2b324fc
Digvijay Singh Rathore <59700645+digvijaysrathore@users.noreply.github.com>
2020-02-29 13:15:39 +0530

    Flex Serial Monitoring

--- flex_sensor_analog_reading.ino
+++ flex_sensor_analog_reading.ino
@@ -2,22 +2,33 @@ const int flex1 = 36;
 const int flex2 = 39;
 const int flex3 = 34;
 const int flex4 = 35;
-int Value =0;
+
 void setup() {
-Serial.begin(115200);
+Serial.begin(9600); //select 9600 as baud rate inserial monitor.
 pinMode(flex1,INPUT);
 pinMode(flex2,INPUT);
 pinMode(flex3,INPUT);
 pinMode(flex4,INPUT);
-
-delay (1000);
+//all the flex sensors are now defined as INPUT.
 }
 
 void loop() {
-  
+  //storing sensor readings to variables.
+  int value1 = analogRead(flex1)
+  int value2 = analogRead(flex2)
+  int value3 = analogRead(flex3)
+  int value4 = analogRead(flex4)
 
-Serial.println(analogRead(39));//2
-Serial.println(analogRead(34));//3
-Serial.println(analogRead(35));//4
-delay(1000);
-}
+ //plotting sensor values to serial monitor by fetching them from variables.
+Serial.printIn("Flex 1: ");
+Serial.println(flex1);//1
+delay(500);
+Serial.printIn("Flex 2: ");
+Serial.println(flex2);//2
+delay(500);
+Serial.printIn("Flex 3: ");
+Serial.println(flex3);//3
+delay(500);
+Serial.printIn("Flex 1: ");
+Serial.printIn(flex4);//4
+delay(2000);
